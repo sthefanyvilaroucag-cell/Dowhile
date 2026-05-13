99d3a420971e566c9fef5d3368577b03e5c352c9
sthefanyvilaroucag-cell <sthefanyvilarouca.g@gmail.com>
2026-05-13 17:09:11 -0300

    dowhile

--- src/Ex06.java
+++ src/Ex06.java
@@ -1,3 +1,19 @@
 void main() {
-    
+    Scanner sc = new Scanner(System.in);
+    int escolha = 0;
+    int numeroum = 1;
+    int numerodois = 2;
+    int numerotres = 3;
+     do { IO.println("escolha um numero");
+
+         escolha = sc.nextInt();
+        if (escolha == numeroum){
+            IO.println("olá");
+        } else if (escolha == numerodois) {
+            IO.println("aaaaaa eu cai na merda");
+        }else {
+            IO.println("saiu");}
+
+
+     }while (escolha != 3);
 }
