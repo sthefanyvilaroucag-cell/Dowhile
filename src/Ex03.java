b19aec33fee8eaac0a6d67a836fab1cc9996e10b
sthefanyvilaroucag-cell <sthefanyvilarouca.g@gmail.com>
2026-05-13 16:49:59 -0300

    dowhile

--- src/Ex03.java
+++ src/Ex03.java
@@ -3,7 +3,7 @@ void main() {
     int senha;
 
     do {
-        IO.println("digite a senha: ");
+        IO.println("digite  a  senha: ");
         senha = sc.nextInt();
     } while (senha != 1234);
     IO.println("Numero correto");
